1b2ea7310e2d6759432bf67587de9f572387804b
LuDotDoc <17019043@uhi.ac.uk>
2020-09-07 21:24:50 +0100

    Tutorial 1a complete

--- Week1/Tutorial1/src/Tutorial1.java
+++ Week1/Tutorial1/src/Tutorial1.java
@@ -1,27 +1,48 @@
 import java.util.Scanner;
 
 public class Tutorial1 {
-    public static void main(String[] args)
-    {
-        int value=0;
-        for (int x=0;x<40;x++)
-        {
-            value = (int)(Math.random()*10) + 1;
-            System.out.print(value+" ");
+    public static void main(String[] args) {
+        Scanner kboard = new Scanner(System.in);
+        String choice;
+        int card1 = 0;
+        int card2 = 0;
+        int newCard;
+        int total;
+
+
+
+        for (int x=0;x<1;x++) {
+            card1 = (int) (Math.random() * 10) + 1;
+//            System.out.println(card1);
         }
 
-        Scanner kboard = new Scanner(System.in);
-        String choice ="";
-        System.out.print("Do you wish to continue y/n? ");
+        for (int x=0;x<1;x++)   {
+            card2 = (int)(Math.random()*10) + 1;
+//            System.out.println(card2);
+        }
+
+        total = card1 + card2;
+        System.out.println("The Object of the game is to score between 19 and 21");
+        System.out.print("You have " + total + "," + " do you want another card y/n? ");
         choice = kboard.next();
         while (choice.equalsIgnoreCase("y"))
         {
-            System.out.println("Hello");
-            System.out.print("Do you wish to continue y/n? ");
-            choice = kboard.next();
+            for (int x=0;x<1;x++) {
+                newCard = (int) (Math.random() * 10) + 1;
+                System.out.println(newCard);
+                total = total + newCard;
+                System.out.print("You have " + total + "," + " do you want another card y/n? ");
+                choice = kboard.next();
+            }
         }
-        System.out.println("Bye");
 
+        if (total >= 19 && total <= 21)   {
+            System.out.println("You have won!");
+        }else{
+            System.out.println("You have lost, please try again");
+        }
     }
-
 }
+
+
+
