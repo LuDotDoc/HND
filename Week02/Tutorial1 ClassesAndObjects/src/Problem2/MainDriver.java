1eb38e5602e24a5f251557dbc626c283fc4107a8
LuDotDoc <17019043@uhi.ac.uk>
2020-09-18 00:09:49 +0100

    Almost finished problem 2

---
+++ Week02/Tutorial1 ClassesAndObjects/src/Problem2/MainDriver.java
@@ -0,0 +1,46 @@
+package Problem2;
+
+import java.util.Scanner;
+
+/*************************************************************************
+ *                           Luke Docwra 17019043                         *
+ *                             2020 HND Tutorial                          *
+ *                               Problem2                                 *
+ *************************************************************************/
+
+public class MainDriver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter 1 - Rectangle Class, 2 - For Circle Class or 3 - To Exit");
+        int choice = scanner.nextInt();
+
+        while(choice != 3) {
+            switch (choice) {
+                case 1:
+                    Rectangle r = new Rectangle();
+                    r.displayInformation();
+                    System.out.println("Please enter 1 - Rectangle Class, 2 - For Circle Class or 3 - To Exit");
+                    choice = scanner.nextInt();
+                    break;
+                case 2:
+                    Circle c = new Circle();
+                    c.displayInformation();
+                    System.out.println("Please enter 1 - Rectangle Class, 2 - For Circle Class or 3 - To Exit");
+                    choice = scanner.nextInt();
+                    break;
+
+                case 3:
+                    System.out.println("Exiting now");
+
+                    break;
+                default:
+                    System.out.println("Please enter 1,2 or 3");
+                    choice = scanner.nextInt();
+                    break;
+            }
+        }
+    }
+}
+
+
